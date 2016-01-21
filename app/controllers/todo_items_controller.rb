e6287dfa4e7a5b742fc4baf3ce1333cb529df945
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-01-21 16:19:42 +0100

    Add check if done

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -7,7 +8,6 @@ class TodoItemsController < ApplicationController
 	end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
       flash[:success] = "Todo list item was deleted."
     else
@@ -16,11 +16,21 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  # Complete item
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: 'Todo item completed.'
+  end
+
 	private	
 
 		def set_todo_list
 			@todo_list = TodoList.find(params[:todo_list_id])
-		end
+    end
+
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
 
 		def todo_item_params
 			params[:todo_item].permit(:content)
